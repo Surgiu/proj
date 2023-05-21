7840fa25d733d12b32ab11d6f52f50d51d5452a3
鼠鼠 <2824174663@qq.com>
2023-05-21 17:33:27 +0800

    model吃子bug fix

---
+++ src/controller/User.java
@@ -0,0 +1,28 @@
+package controller;
+
+public class User {
+    private String name;
+    private String psd;
+
+    public User(String name, String psd) {
+        this.name = name;
+        this.psd = psd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPsd() {
+        return psd;
+    }
+
+    public void setPsd(String psd) {
+        this.psd = psd;
+    }
+
+}
