d16ba57947d27b3f6049fd30d818a68026292464
鼠鼠 <2824174663@qq.com>
2023-05-21 21:37:22 +0800

    “内鬼”问题未解决

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -51,10 +51,10 @@ public class GameController implements GameListener {
     // click an empty cell
     @Override
     public void onPlayerClickCell(ChessboardPoint point, CellComponent component) {
-        if (selectedPoint != null && model.isValidMove(selectedPoint, point)) {
+        if (selectedPoint != null) {
+            model.escapeTrap(selectedPoint, point);
             model.moveChessPiece(selectedPoint, point);
             model.inTrap(point);
-            model.escapeTrap(selectedPoint, point);
             view.setChessComponentAtGrid(point, view.removeChessComponentAtGrid(selectedPoint));
             if (model.inDens(point)) {
                 return;
@@ -68,26 +68,27 @@ public class GameController implements GameListener {
     // click a cell with a chess
     @Override
     public void onPlayerClickChessPiece(ChessboardPoint point, ChessComp component) {
-        if (selectedPoint == null) {
+        if (selectedPoint == null) {//如果还没被选中，那么就让它被选中
             if (model.getChessPieceOwner(point).equals(currentPlayer)) {
                 selectedPoint = point;
                 component.setSelected(true);
                 component.repaint();
             }
-        } else if (selectedPoint.equals(point)) {
+        } else if (selectedPoint.equals(point)) {//如果放到自己的位置，就放弃选中
             selectedPoint = null;
             component.setSelected(false);
             component.repaint();
         } else {
-            if (model.isValidCapture(selectedPoint, point)) {
+            if (model.isValidCapture(selectedPoint, point)) {//如果是有效动作，进行相应操作
                 model.captureChessPiece(selectedPoint, point);
                 view.removeChessComponentAtGrid(point);
                 view.setChessComponentAtGrid(point, view.removeChessComponentAtGrid(selectedPoint));
                 selectedPoint = null;
+                swapColor();
             } else if (!model.isValidCapture(selectedPoint, point)) {
+                selectedPoint = null;
                 System.err.println("Illegal capture");
             }
-            swapColor();
             view.repaint();
         }
     }
