ab8663ea24f7cd1d6ffd40b4d1ab2bf0f968b995
鼠鼠 <2824174663@qq.com>
2023-05-28 08:53:01 +0800

    棋子显示优化

--- src/view/ChessComp.java
+++ src/view/ChessComp.java
@@ -39,6 +39,8 @@ public class ChessComp extends JComponent {
         g2.setFont(font);
         g2.setColor(getOwner().getColor());
         g2.drawString(name, getWidth() / 4, getHeight() * 5 / 8);
+        BasicStroke b2 = new BasicStroke(3);
+        g2.setStroke(b2);
         g2.drawOval(0, 0, getWidth(), getHeight());
         if (isSelected()) { // Highlights the model if selected.
             BasicStroke circle = new BasicStroke(140.0f);
